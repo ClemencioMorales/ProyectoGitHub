eb09ef59906bfcf813ba36c3af2c4299a5094477
ClemencioMorales <clemencio.morales.lucas@alumnos.upm.es>
2013-10-20 10:49:33 +0200

    Primer commit.

---
+++ src/es/miw/upm/git/Clase1.java
@@ -0,0 +1,14 @@
+package es.miw.upm.git;
+
+public class Clase1 {
+
+	public void print(String msg){
+		System.out.println("Mensaje: "+msg);
+	}
+	
+	public static void main(String[] args) {
+		Clase1 clase1 = new Clase1();
+		clase1.print("Prueba");
+	}
+
+}
